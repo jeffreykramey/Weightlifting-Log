ed8d6dde6e2d27dea148ef68a6ef72d9ce988c0d
Jeff Ramey <70029983+jeffreykramey@users.noreply.github.com>
2021-02-06 15:45:56 -0700

    Delete CoinGame directory

--- CoinGame/BadCoin.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  BadCoin.cpp
-//  CoinGame
-//
-//  Created by Jeff Ramey on 9/23/20.
-//
-
-#include "BadCoin.hpp"
--- CoinGame/projectile.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  projectile.cpp
-//  CoinGame
-//
-//  Created by Jeff Ramey on 9/23/20.
-//
-
-#include "projectile.hpp"
